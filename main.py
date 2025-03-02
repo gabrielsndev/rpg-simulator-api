b1a62d42525b55bf507dcd636df9ab58331b38ff
Gabriel de Souza <gabrielsndev@gmail.com>
2025-03-02 11:55:36 -0300

    Rolada de dados
    Terminando a rota para rolar dados, e também criando o requirements.txt

--- main.py
+++ main.py
@@ -6,35 +6,62 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Bem-vindo ao RPG Simulator API!'}
+  
 
-# @app.get('/roll/expression/{expressao}')
-# def roll_multiple_dice(expressao: str):
-#     values = expressao.split('d')
-    
-#     if len(values) < 2:
-#         return {'error': 'Input  inválido, utilize o modelo {quantidade}d{lado}.'}
-    
-#     quantidade = int(values[0])
-#     lado = int(values[1])
-    
-#     if lado < 2:
-#         return {'error': 'Número de lados deve ser maior que 2.'}
-    
-#     if quantidade < 1:
-#         return {'error': 'Número de dados deve ser maior ou igual a 1.'}
-    
-#     resultado = 0
-#     for i in range(quantidade):
-#         resultado += random.randint(1, lado)
+@app.get('/roll/{dado}')
+def roll_dice(dado: str):
+    import random
 
-#     return {'dado': f'{quantidade}d{lado}', 'total': resultado}    
+    acrescimo = 0 
+    decrescimo = 0
 
-@app.get('/roll/{lado}')
-def roll_dice(lado: str):
-    if lado < 2:
-        return {'error': 'Número de lados deve ser maior que 2.'}
-    
-    resultado =  random.randint(1, lado)
-    return{'dado': f'd{lado}', 'resultado': resultado}
+    if 'd' in dado:
+        dado = dado.split('d')
+    else:
+        return'Valor informado é inválido'
+        
+    if len(dado) != 2:
+        return'Formato informado é inválido'
+        
+    try: quantidade_dados = int(dado[0])
+    except: return'A quantidade de dados informada é inválida'
 
+    lado_dados = dado[1]
+
+    if '+' in lado_dados:
+        try:
+            dados = lado_dados.split('+')
+            lado_dados = int(dados[0])
+            acrescimo = int(dados[1])
+        except: return'Valor inválido para acrescimo dos dados'
+        
+    elif '-' in lado_dados:
+        try:
+            dados = lado_dados.split('-')
+            lado_dados = int(dados[0])
+            decrescimo = int(dados[1])
+        except: return'Valor inválido para decrescimo dos dados'
+    else:
+        try:
+            lado_dados = int(lado_dados)
+        except: return'Valor inválido para lado dos dados'
+
+
+
+    if quantidade_dados < 1 or lado_dados < 2:
+        return'Valor inválido para quantidade de dados ou lado dos dados.'
+        
+    total = 0
+    for i in range(quantidade_dados):
+        dado_atual = random.randint(1, lado_dados)
+        total += dado_atual
+
+    if acrescimo:
+        total += acrescimo
+        
+    elif decrescimo:
+        total -= decrescimo
+        
+        
+    return total
 
--- testes.py
+++ testes.py
@@ -1,47 +1,55 @@
 import random
 
-dado = '2d20-3'
-acrescimo = 0 
-decrescimo = 0
+# dado = '2d20-3'
+# acrescimo = 0 
+# decrescimo = 0
 
-if 'd' in dado:
-    dado = dado.split('d')
-else:
-    print('Valor informado é inválido.')
+# if 'd' in dado:
+#     dado = dado.split('d')
+# else:
+#     return'Valor informado é inválido'
     
-if len(dado) != 2:
-    print('Valor informado é inválido.')
+# if len(dado) != 2:
+#     return'Formato informado é inválido'
     
-quantidade_dados = int(dado[0])
-lado_dados = dado[1]
+# try: quantidade_dados = int(dado[0])
+# except: return'A quantidade de dados informada é inválida'
 
-if '+' in lado_dados:
-    dados = lado_dados.split('+')
-    lado_dados = int(dados[0])
-    acrescimo = int(dados[1])
+# lado_dados = dado[1]
+
+# if '+' in lado_dados:
+#     try:
+#         dados = lado_dados.split('+')
+#         lado_dados = int(dados[0])
+#         acrescimo = int(dados[1])
+#     except: return'Valor inválido para acrescimo dos dados'
     
-elif '-' in lado_dados:
-    dados = lado_dados.split('-')
-    lado_dados = int(dados[0])
-    decrescimo = int(dados[1])
-else:
-    lado_dados = int(lado_dados)
+# elif '-' in lado_dados:
+#     try:
+#         dados = lado_dados.split('-')
+#         lado_dados = int(dados[0])
+#         decrescimo = int(dados[1])
+#     except: return'Valor inválido para decrescimo dos dados'
+# else:
+#     try:
+#         lado_dados = int(lado_dados)
+#     except: return'Valor inválido para lado dos dados'
 
 
 
-if quantidade_dados < 1 or lado_dados < 2:
-    print('Valor inválido para quantidade de dados ou lado dos dados.')
+# if quantidade_dados < 1 or lado_dados < 2:
+#     return'Valor inválido para quantidade de dados ou lado dos dados.'
     
-total = 0
-for i in range(quantidade_dados):
-    dado_atual = random.randint(1, lado_dados)
-    total += dado_atual
+# total = 0
+# for i in range(quantidade_dados):
+#     dado_atual = random.randint(1, lado_dados)
+#     total += dado_atual
 
-if acrescimo:
-    total += acrescimo
+# if acrescimo:
+#     total += acrescimo
     
-elif decrescimo:
-    total -= decrescimo
+# elif decrescimo:
+#     total -= decrescimo
     
     
-print(total)
+# return total
